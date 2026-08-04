56b9c595a3344101a74230c832f8552a2c780a02
mohith1306 <tatinenimohith@gmail.com>
2026-08-04 22:58:19 +0530

    Time: 8 ms (12.00%) | Memory: 47.4 MB (11.76%) - LeetSync

---
+++ 4107-find-missing-elements/find-missing-elements.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        Arrays.sort(nums);
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        List<Integer> ans=new ArrayList<>();
+        for(int i=nums[0];i<=nums[nums.length-1];i++){
+            if(!map.containsKey(i))ans.add(i);
+        } 
+        return ans;
+    }
+}
